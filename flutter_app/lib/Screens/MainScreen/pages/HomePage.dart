0837cf234628d17299ada1ed2c8d6705a7dc8bd4
LaggeR <vo.binh224@gmail.com>
2021-06-30 20:38:25 +0700

    add animation for chatlist when scroll

--- flutter_app/lib/Screens/MainScreen/pages/HomePage.dart
+++ flutter_app/lib/Screens/MainScreen/pages/HomePage.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app/Screens/MainScreen/pages/NotificationsPage.dart';
 import 'package:flutter_app/Screens/MainScreen/pages/ProfilePage.dart';
 import 'package:flutter_app/Screens/MainScreen/pages/SearchPage.dart';
@@ -7,6 +7,7 @@ import 'package:flutter_app/Screens/MainScreen/pages/TimelinePage.dart';
 import 'package:flutter_app/Screens/MainScreen/pages/MessengerPage.dart';
 import 'package:flutter_app/constants.dart';
 import 'package:flutter_app/main.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class NavigationItem {
   const NavigationItem(this.title, this.icon);
@@ -30,57 +31,62 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
+  DateTime currentBackPressTime;
+
   PageController _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        children: <Widget>[
-          TimelinePage(),
-          SearchPage(),
-          MessengerPage(),
-          NotificationsPage(),
-          ProfilePage(userId: currentUserModel.id)
-        ],
-      ),
-      // body: IndexedStack(
-      //   children: <Widget>[
-      //     TimelinePage(),
-      //     SearchPage(),
-      //     MessengerPage(),
-      //     NotificationsPage(),
-      //     ProfilePage(userId: currentUserModel.id)
-      //   ],
-      //   index: _currentIndex,
-      // ),
-      //body: _listPage[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _pageController.jumpToPage(index);
-          });
-        },
-        items: allNavigationItems.map((NavigationItem navigationItem) {
-          return BottomNavigationBarItem(
-              icon: Icon(
-                navigationItem.icon,
-                color: appPrimaryColor,
-              ),
-              backgroundColor: appBackgroundLightColor,
-              title: Text(
-                navigationItem.title,
-                style: TextStyle(color: appPrimaryColor),
-              ));
-        }).toList(),
-      ),
-    );
+    return WillPopScope(
+        child: Scaffold(
+          body: PageView(
+            controller: _pageController,
+            onPageChanged: (int index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            children: <Widget>[
+              TimelinePage(),
+              SearchPage(),
+              MessengerPage(),
+              NotificationsPage(),
+              ProfilePage(userId: currentUserModel.id)
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (int index) {
+              setState(() {
+                _pageController.jumpToPage(index);
+              });
+            },
+            items: allNavigationItems.map((NavigationItem navigationItem) {
+              return BottomNavigationBarItem(
+                  icon: Icon(
+                    navigationItem.icon,
+                    color: appPrimaryColor,
+                  ),
+                  backgroundColor: appBackgroundLightColor,
+                  title: Text(
+                    navigationItem.title,
+                    style: TextStyle(color: appPrimaryColor),
+                  ));
+            }).toList(),
+          ),
+        ),
+        onWillPop: onBackPress);
+  }
+
+  Future<bool> onBackPress() {
+    DateTime now = DateTime.now();
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime) > Duration(seconds: 2)) {
+      currentBackPressTime = now;
+      Fluttertoast.showToast(msg: "Press again to exit app");
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
   }
 }
