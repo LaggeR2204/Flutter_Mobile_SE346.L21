afa64cd7d76f8b6c910b351f32617853b5f84d4c
LaggeR <vo.binh224@gmail.com>
2021-07-01 20:29:26 +0700

    add search acc in searchpage

--- flutter_app/lib/Screens/MainScreen/pages/SearchPage.dart
+++ flutter_app/lib/Screens/MainScreen/pages/SearchPage.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_app/Screens/MainScreen/pages/ProfilePage.dart';
 import 'package:flutter_app/constants.dart';
+import 'package:flutter_app/models/Post.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -12,22 +15,38 @@ class SearchPageState extends State<SearchPage>
   final double appBarHeight = AppBar().preferredSize.height;
   TabController _tabController;
   String searchHintText;
+  TextEditingController _searchTermController;
+  String searchTerm = "";
 
   @override
   void initState() {
     _tabController = new TabController(length: 3, vsync: this);
+    _searchTermController = new TextEditingController();
     _tabController.addListener(TabControllerListener);
+    _searchTermController.addListener(SearchControllerListener);
     searchHintText = 'Search';
     super.initState();
   }
 
   @override
   void dispose() {
+    _searchTermController.removeListener(SearchControllerListener);
     _tabController.removeListener(TabControllerListener);
     super.dispose();
   }
 
+  SearchControllerListener() {
+    print('[LISTENER]');
+    setState(() {
+      searchTerm = _searchTermController.value.text;
+    });
+  }
+
   TabControllerListener() {
+    if (_tabController.indexIsChanging) {
+      _searchTermController.clear();
+    }
+
     if (_tabController.index == 0) {
       setState(() {
         searchHintText = 'Search';
@@ -40,6 +59,176 @@ class SearchPageState extends State<SearchPage>
       setState(() {
         searchHintText = 'Search places';
       });
+    }
+  }
+
+  Container buildSearchResult() {
+    final Size size = MediaQuery.of(context).size;
+    QuerySnapshot temp;
+
+    if (_tabController.index == 1) {
+      // search accounts
+      return Container(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("users")
+              .where("displayName", isGreaterThanOrEqualTo: searchTerm)
+              .snapshots(),
+          builder: (context, snapshot) {
+            return (searchTerm == "")
+                ? Center(
+                    child: Text("Looking for soneone"),
+                  )
+                : (snapshot.connectionState == ConnectionState.waiting)
+                    ? Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        itemCount: snapshot.data.docs.length,
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot doc = snapshot.data.docs[index];
+                          var data = doc.data();
+                          return FlatButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return ProfilePage(userId: data['id']);
+                                }));
+                              },
+                              child: Column(children: <Widget>[
+                                SizedBox(
+                                  height: size.height * 0.01,
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    SizedBox(
+                                      width: (data['photoUrl'] != "") ? 25 : 20,
+                                    ),
+                                    (data['photoUrl'] != "")
+                                        ? CircleAvatar(
+                                            radius: size.width * 0.06,
+                                            backgroundImage:
+                                                NetworkImage(data['photoUrl']),
+                                          )
+                                        : Image.asset(
+                                            "assets/images/defaultProfileImage.png",
+                                            width: size.width * 0.14,
+                                            height: size.width * 0.14,
+                                            fit: BoxFit.fitHeight),
+                                    SizedBox(
+                                      width: (data['photoUrl'] != "") ? 25 : 20,
+                                    ),
+                                    Text(
+                                      data['displayName'],
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.normal),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: size.height * 0.01,
+                                ),
+                              ]));
+                        },
+                      );
+          },
+        ),
+      );
+    } else if (_tabController.index == 2) {
+      //search places
+      return Container(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("posts")
+              .where("location", isGreaterThanOrEqualTo: searchTerm)
+              //.orderBy("likes", )
+              .snapshots(),
+          builder: (context, snapshot) {
+            return (searchTerm == "")
+                ? Center(
+                    child: Text("Looking for somewhere"),
+                  )
+                : (snapshot.connectionState == ConnectionState.waiting)
+                    ? Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        itemCount: snapshot.data.docs.length,
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot doc = snapshot.data.docs[index];
+                          var data = doc.data();
+                          return FlatButton(
+                              onPressed: () {
+                                // ImagePost temp = ImagePost.fromDocument(doc);
+                                // Navigator.of(context).push(
+                                //     MaterialPageRoute<bool>(
+                                //         builder: (BuildContext context) {
+                                //   return Center(
+                                //     child: Scaffold(
+                                //         appBar: AppBar(
+                                //           automaticallyImplyLeading: true,
+                                //           brightness: Brightness.dark,
+                                //           flexibleSpace: Container(
+                                //             decoration: BoxDecoration(
+                                //               gradient: LinearGradient(
+                                //                 begin: Alignment.centerLeft,
+                                //                 end: Alignment.centerRight,
+                                //                 colors: <Color>[
+                                //                   appPrimaryColor,
+                                //                   appPrimaryColor2
+                                //                 ],
+                                //               ),
+                                //             ),
+                                //           ),
+                                //           title: Text('Photo'),
+                                //         ),
+                                //         body: ListView(
+                                //           children: <Widget>[
+                                //             Container(
+                                //               child: temp,
+                                //             ),
+                                //           ],
+                                //         )),
+                                //   );
+                                // }));
+                              },
+                              child: Column(children: <Widget>[
+                                SizedBox(
+                                  height: size.height * 0.01,
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    SizedBox(
+                                      width: (data['mediaUrl'] != "") ? 25 : 20,
+                                    ),
+                                    (data['mediaUrl'] != "")
+                                        ? CircleAvatar(
+                                            radius: size.width * 0.06,
+                                            backgroundImage:
+                                                NetworkImage(data['mediaUrl']),
+                                          )
+                                        : Image.asset(
+                                            "assets/images/defaultProfileImage.png",
+                                            width: size.width * 0.14,
+                                            height: size.width * 0.14,
+                                            fit: BoxFit.fitHeight),
+                                    SizedBox(
+                                      width: (data['mediaUrl'] != "") ? 25 : 20,
+                                    ),
+                                    // Text(
+                                    //   data['ownerId'],
+                                    //   style: TextStyle(
+                                    //       fontSize: 20,
+                                    //       fontWeight: FontWeight.normal),
+                                    // ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: size.height * 0.01,
+                                ),
+                              ]));
+                        },
+                      );
+          },
+        ),
+      );
     }
   }
 
@@ -82,6 +271,7 @@ class SearchPageState extends State<SearchPage>
             borderRadius: BorderRadius.circular(29),
           ),
           child: TextField(
+            controller: _searchTermController,
             cursorColor: appPrimaryColor,
             decoration: InputDecoration(
               hintStyle: TextStyle(fontSize: appBarHeight * 0.3),
@@ -94,20 +284,93 @@ class SearchPageState extends State<SearchPage>
           ),
         ),
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          Center(
-            child: Text("search recent"),
-          ),
-          Center(
-            child: Text("search acc"),
-          ),
-          Center(
-            child: Text("search places"),
-          ),
-        ],
-      ),
+      body: buildSearchResult(),
+      // body: TabBarView(
+      //   controller: _tabController,
+      //   children: [
+      //     Center(
+      //       child: Text("search recent"),
+      //     ),
+
+      //     Center(
+      //       child: Text("search places"),
+      //     ),
+      //   ],
+      // ),
     );
+  }
+
+  Widget buildSearchAccountsPage(BuildContext context, String searchTerm) {
+    final Size size = MediaQuery.of(context).size;
+    if (searchTerm != "" && searchTerm != null) {
+      return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection("users")
+            //.where("id", isNotEqualTo: currentUserModel.id)
+            .where("displayName", isGreaterThanOrEqualTo: searchTerm)
+            .snapshots(),
+        builder: (context, snapshot) {
+          return (snapshot.connectionState == ConnectionState.waiting)
+              ? Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot doc = snapshot.data.docs[index];
+                    var data = doc.data();
+                    return FlatButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return ProfilePage(userId: data['id']);
+                          }));
+                        },
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        //   return Chat(
+                        //       peerId: data['id'],
+                        //       peerAvatar: data['photoUrl'],
+                        //       peerName: data['displaynName']); //Mở tin nhắn của user này
+                        // })),
+                        child: Column(children: <Widget>[
+                          SizedBox(
+                            height: size.height * 0.01,
+                          ),
+                          Row(
+                            children: <Widget>[
+                              SizedBox(
+                                width: (data['photoUrl'] != "") ? 25 : 20,
+                              ),
+                              (data['photoUrl'] != "")
+                                  ? CircleAvatar(
+                                      radius: size.width * 0.06,
+                                      backgroundImage:
+                                          NetworkImage(data['photoUrl']),
+                                    )
+                                  : Image.asset(
+                                      "assets/images/defaultProfileImage.png",
+                                      width: size.width * 0.14,
+                                      height: size.width * 0.14,
+                                      fit: BoxFit.fitHeight),
+                              SizedBox(
+                                width: (data['photoUrl'] != "") ? 25 : 20,
+                              ),
+                              Text(
+                                data['displayName'],
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: size.height * 0.01,
+                          ),
+                        ]));
+                  },
+                );
+        },
+      );
+    } else {
+      return Container();
+    }
   }
 }
